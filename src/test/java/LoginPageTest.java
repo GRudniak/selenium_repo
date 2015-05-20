51db6158bb2c579d62865c741117ac531c243ce2
GRudniak <grzegorzrudniak@gmail.com>
2015-05-20 20:24:41 +0200

    first repo

---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Grzegorz on 2015-05-20.
+ */
+
+
+public class LoginPageTest {
+    @Test
+    public void should_open_login_page(){
+        System.setProperty("webdriver.chrome.driver", "D:\\Software_Tools\\webdriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+    }
+}
